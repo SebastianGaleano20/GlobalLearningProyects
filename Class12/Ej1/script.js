804ee481bb1264dd2ffc777d7e93628110375d51
Sebastian <sebascarp9@gmail.com>
2024-05-02 21:11:32 -0300

    js create

--- Class12/Ej1/script.js
+++ Class12/Ej1/script.js
@@ -1,3 +1,21 @@
-const firstNameinput = document.getElementById('firstName');
+//Generamos una funcion que se ejecute una vez cargado todo el documento html.
+document.addEventListener('DOMContentLoaded', ()=>{
+document.getElementById('formulary').addEventListener('submit', validateForm)    
+//Generamos una funcion que obtenga el formulario y aguarde al evento submit para validar los datos
+})
+//Creamos una funcion que valide los datos del formulario
+const validateForm = (event) =>{
+  //Detenemos el envio del formulario
+  event.preventDefault(); 
+  let nameValue = document.getElementById('firstName');
+  console.log(nameValue)
+  let firstName = nameValue.value
 
+  if(firstName.length === 0){
+    alert('El nombre no puede contener 0 caracteres')
+    return;
+  }
+  this.submit(); 
+}
 
+console.log(validateForm())
