fbad3b4813d871076ef9ce91b9082f6ed67f497f
Sebastian <sebascarp9@gmail.com>
2024-05-10 21:50:34 -0300

    Challenge finished

--- Class14/Challenge/index.js
+++ Class14/Challenge/index.js
@@ -4,12 +4,12 @@ const timer = (time) => {
     //Al ser var el valor se pierde por lo tanto imprime undefined en pantalla
     for (var i = 0; i < time.length; i++) {
         //Creamos una funcion temporal para capturar el valor para imprimirlo en pantalla
-        ((index)=>{
+        ((index)=>{ //Enviamos por parametro el index para que cada vuelta del bucle se guarde dentro de esta funcion
             setTimeout(() => {
-                console.log(time[index]);
-            }, (index + 1 ) * 2000);
-        })(i);
+                console.log(time[index]); //Utilizamos el valor del parametro para mostrarlo en consola
+            }, index * 2000); //En cada vuelta tarda 2 segundos en mostrar el numero en consola
+        })(i); //Enviamos el valor de i como parametro
 } 
 };
 
-console.log(timer(numbers));
+console.log(timer(numbers)); 
