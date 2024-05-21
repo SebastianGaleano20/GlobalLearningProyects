f80d65ad500ec64fc7be926bcd184bc2c507bfaf
Sebastian Galeano <sebascarp9@gmail.com>
2024-05-21 09:39:17 -0300

    Exercise 14-2 Class 15 finished

--- Class15/index.js
+++ Class15/index.js
@@ -9,16 +9,20 @@ const PORT = process.env.PORT  || 3000;
 //Configuramos EJS
 app.set('view engine','ejs');
 
+//Configuramos el archivo estatico a utilizar
+app.use(express.static('public'));
+
 //configuramos el metodo get 
 app.get('/',(_request,response) => {
-    response.render('index')
+    response.render('./index')
 })
 
 //Configuracion para error 404
 app.get('*',(_request,response)=>{
-    response.render('error404');
+    response.render('./error404');
 })
 
+//Puerto en escucha del servidor creado
 app.listen(PORT,() =>{
     console.log(`App listening on port ${PORT}`);
 })
