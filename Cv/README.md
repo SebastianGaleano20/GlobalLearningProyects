74bfa7b66e79cfe891794ecc584e8a688f92c62d
Sebastian Galeano <sebascarp9@gmail.com>
2024-05-22 09:54:19 -0300

    server and md file included

---
+++ Cv/README.md
@@ -0,0 +1,14 @@
+# Proyecto Cv 
+*Repositorio para actualizar curriculum vitae personal*
+
+## Contenido
+- [x] Sobre mi
+- [x] Informacion academica
+- [x] Informacion laboral
+- [x] Datos personales 
+- [x] Formulario de contacto
+- [x] Añadir servidor
+- [] Validacion de datos para formulario
+- [] Pagina responsive
+- [] Estilos del cv
+- [] Logo en pestaña
---
+++ Cv/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/',(_request,response) => {
+    response.render('./index')
+})
+
+app.listen(PORT,() =>{
+    console.log(`App listening on port ${PORT}`);
+})
