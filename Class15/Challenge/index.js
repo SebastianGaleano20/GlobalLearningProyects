c23e9b9f5aa71149471b55f800960bb7a5becfb4
Sebastian Galeano <sebascarp9@gmail.com>
2024-05-23 10:36:23 -0300

    Server configuration

--- Class15/Challenge/index.js
+++ Class15/Challenge/index.js
@@ -1,17 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import {v4 as uuidV4} from 'uuid';
 const app = express();
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
 app.get('/',(_request,response) => {
-    response.render('./index')
+    response.redirect(uuidV4());
+})
+app.get('/:index', (request, response)=>{
+    response.render('index', { indexId: request.params.index});
 })
 
 app.listen(PORT,() =>{
