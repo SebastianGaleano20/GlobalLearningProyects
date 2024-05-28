28fbe4cfb02062c2fe5ed493708a5fb3c911cb16
Sebastian Galeano <sebascarp9@gmail.com>
2024-05-28 10:00:30 -0300

    Data fuction

---
+++ Cv/public/data-function.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const btnData = document.getElementById('data-btn');
+    const btnHobbie = document.getElementById('hobbie-btn');
+    const btnEdu = document.getElementById('education-btn');
+    const infoData = document.getElementById('txt-data');
+    const infoHobbie = document.getElementById('txt-hobbie');
+    const infoEdu = document.getElementById('txt-edu');
+
+    btnData.addEventListener('click', ()=>{
+        showTxt(infoData);
+    })
+    btnHobbie.addEventListener('click', ()=>{
+        showTxt(infoHobbie);
+    })
+    btnEdu.addEventListener('click', ()=>{
+        showTxt(infoEdu);
+    })
+
+    const showTxt = (info) => {
+         const allTxt = document.querySelectorAll('.txt');
+         allTxt.forEach((p)=>{
+            p.style.display = 'none' 
+         })
+         info.style.display = 'block'
+    }
+})
