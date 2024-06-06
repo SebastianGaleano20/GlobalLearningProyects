33fbf61c7cd4d4de6f246511de35d145b1afab86
Sebastian Galeano <sebascarp9@gmail.com>
2024-06-06 10:44:20 -0300

    Server on

--- Class16/index.js
+++ Class16/index.js
@@ -4,4 +4,17 @@ import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 2018
+const PORT = process.env.PORT || 2018;
+
+app.use(express.static('public'));
+app.use(express.static('assets'));
+
+app.set('view engine','ejs');
+
+app.get('/', (_request,response)=>{
+    response.render('./index')
+});
+
+app.listen(PORT, ()=>{
+    console.log(`App listening on port ${PORT}`);
+});
